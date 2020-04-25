9b846c863f04387bfded4dc7f0c1a89c77515dbb
mikeku <mikekuforgit@gmail.com>
2020-04-25 22:35:03 +0800

    完成TodoCreateView

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TodoListView
+from .views import TodoListView, TodoCreateView
 
 app_name = 'todo'
 urlpatterns = [
     path('', TodoListView.as_view(), name='list'),
+    path('create', TodoCreateView.as_view(), name='create'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from .models import Todo
+from .forms import TodoModelForm
 from django.views.generic import (
-    ListView
+    ListView,
+    CreateView
 )
 
 
 class TodoListView(ListView):
     queryset = Todo.objects.filter(finish=False)  # 指定查詢條件
+
+    # 要傳遞的資料
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = TodoModelForm()
+        return context
+
+    # POST請求時所要做的事情
+    def post(self, request, *args, **kwargs):
+        return TodoCreateView.as_view()(request)
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title']  # 要顯示及儲存的欄位
+    success_url = '/todo'  # 儲存成功後要導向的網址
