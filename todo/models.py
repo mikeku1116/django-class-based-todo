6f56fdcd7869c4f6c3d89f938de16b6e39435457
mikeku <mikekuforgit@gmail.com>
2020-04-25 18:44:41 +0800

    建立Todo資料模型及執行Migration

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    finish = models.BooleanField(default=False)
+    created = models.DateField(default=timezone.now)
