70cfcab4fb91a9089b2c1be84e58c4033dd8f79a
mikeku <mikekuforgit@gmail.com>
2020-04-25 22:41:40 +0800

    完成TodoUpdateView

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TodoListView, TodoCreateView
+from .views import TodoListView, TodoCreateView, TodoUpdateView
 
 app_name = 'todo'
 urlpatterns = [
     path('', TodoListView.as_view(), name='list'),
     path('create', TodoCreateView.as_view(), name='create'),
+    path('update/<int:pk>', TodoUpdateView.as_view(), name='update'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -3,7 +3,8 @@ from .models import Todo
 from .forms import TodoModelForm
 from django.views.generic import (
     ListView,
-    CreateView
+    CreateView,
+    UpdateView
 )
 
 
@@ -24,4 +25,11 @@ class TodoListView(ListView):
 class TodoCreateView(CreateView):
     model = Todo
     fields = ['title']  # 要顯示及儲存的欄位
+    success_url = '/todo'  # 儲存成功後要導向的網址
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title', 'finish']  # 要顯示及修改的欄位
+    template_name = 'todo/todo_update.html'  # 修改樣板
     success_url = '/todo'  # 儲存成功後要導向的網址
